2c49edfb2c94f395d8c41c9604f898945a92aa2b
Callie <96257895+pips90@users.noreply.github.com>
2024-10-06 16:33:39 -0500

    making new master branch

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
+  console.log('this app is running')
   return (
     <div>
       <h2>Welcome to the Habit Tracker</h2>
