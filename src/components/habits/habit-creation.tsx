e92bea77042929ebc64ee4058131bc7ac456df37
Callie <96257895+pips90@users.noreply.github.com>
2024-10-09 15:52:20 -0500

    removed redundant useEffect

--- src/components/habits/habit-creation.tsx
+++ src/components/habits/habit-creation.tsx
@@ -32,11 +32,6 @@ const HabitCreation = ({ createHabit }: HabitCreationProps) => {
   const habits = useAppSelector((state: RootState) => state.habitCreation);
   const { register, handleSubmit, reset } = useForm<Habit>();
 
-  // useEffect to log all habits whenever they change
-  useEffect(() => {
-    console.log("All habits:", habits);
-  }, [habits]); // Trigger useEffect whenever the habits array changes
-
   const onSubmit: SubmitHandler<Habit> = (newHabit) => {
     // Generate a new unique ID each time a habit is created. TODO: use uuidv4 - something similar for this
     const newCreatedHabit = {
